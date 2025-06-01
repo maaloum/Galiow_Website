0545571aade9afcbd9e8daa2f3d1c60d0b329edb
maaloum <maaloumali1@gmail.com>
2025-05-31 22:34:42 -0400

    build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
-plugins: ['import'],
-extends: [
-  'plugin:import/errors',
-  'plugin:import/warnings',
-  'plugin:import/typescript', // optional if using TS
-],
-rules: [{
-  'import/no-extraneous-dependencies': 'error',
-  'import/extensions': 'error',
-}]
+module.exports = {
+  plugins: ['import'],
+  extends: [
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript', // optional if using TypeScript
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 'error',
+    'import/extensions': 'error',
+  },
+};
